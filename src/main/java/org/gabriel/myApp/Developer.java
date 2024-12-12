edaffa07e9e971bdd6806df6f8e4f5ab0984baf5
gabriel <gabriell_rsilva@outlook.com>
2024-12-12 10:25:12 -0300

    autowired

--- src/main/java/org/gabriel/myApp/Developer.java
+++ src/main/java/org/gabriel/myApp/Developer.java
@@ -1,14 +1,31 @@
 package org.gabriel.myApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+// Contextualizando - A ideia aqui é que seja um Dev que construa coisas e dependa de ferramentas para isso
 
 @Component
 public class Developer {
 
+    @Autowired                           // injeção de dependência - field injection
+    private Laptop laptop;              // se não informar ao Spring, essa dependência será null
+
+
+//    @Autowired                            constructor injection
+//    public Developer(Laptop laptop){      default - não exige uso explícito do @Autowired em classes que tem apenas um construtor
+//        this.laptop = laptop;
+//    }
+
+
     public void build() {
+
+        laptop.compile();
         System.out.println("Working on Awesome Project");
     }
 
-
-    // A ideia aqui é que seja um Dev que construa coisas e dependa de ferramentas para isso
+//    @Autowired
+//    public void setLaptop(Laptop laptop){     setter injection
+//        this.laptop = laptop;
+//    }
 }
---
+++ src/main/java/org/gabriel/myApp/Laptop.java
@@ -0,0 +1,12 @@
+package org.gabriel.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile() {
+        System.out.println("Compiling with 404 bugs");
+    }
+
+}
